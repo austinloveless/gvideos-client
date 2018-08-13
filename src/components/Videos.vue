2d939bc977a3d6fb3d5c885b5cf24fe2fa8bf01c
Josh <joshaaronlevy@gmail.com>
2018-08-13 15:40:58 -0600

    Failing - Working on video fetch.

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -4,21 +4,21 @@
 
 <script>
 export default {
-  data() {
-    return {
-      videos: [],
-      apiURL: 'https://gvideos-api.herokuapp.com/api/videos',
-    };
-  },
-  mounted() {
-    fetch(this.apiURL)
-      .then(response => response.json())
-      .then(response => {
-        this.videos = response;
-        console.log(response);
-      })
-  },
-}
+  // data() {
+  //   return {
+  //     videos: [],
+  //     apiURL: 'https://gvideos-api.herokuapp.com/api/videos',
+  //   };
+  // },
+  // mounted() {
+  //   fetch(this.apiURL)
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       this.videos = response;
+  //       console.log(response);
+  //     })
+  // },
+};
 </script>
 
 <style scoped>
