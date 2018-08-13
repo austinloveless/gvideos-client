c1cb08728b095fc1a33ad38f85362e61a9928349
leviboenish <leviboenish@gmail.com>
2018-08-13 14:53:21 -0600

    base auth styles

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,44 +1,39 @@
 <template>
-  <div>
+  <div class="container col-3 jumbotron auth">
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group id="exampleInputGroup1"
-                    label="Email address:"
-                    label-for="exampleInput1"
-                    description="We'll never share your email with anyone else.">
-        <b-form-input id="exampleInput1"
-                      type="email"
-                      v-model="form.email"
-                      required
-                      placeholder="Enter email">
-        </b-form-input>
       </b-form-group>
       <b-form-group id="exampleInputGroup2"
-                    label="Your Name:"
+                    label="Username:"
                     label-for="exampleInput2">
+
         <b-form-input id="exampleInput2"
                       type="text"
                       v-model="form.name"
                       required
                       placeholder="Enter name">
         </b-form-input>
+
       </b-form-group>
       <b-form-group id="exampleInputGroup3"
-                    label="Food:"
+                    label="Password:"
                     label-for="exampleInput3">
-        <b-form-select id="exampleInput3"
-                      :options="foods"
+        <b-form-input id="exampleInput3"
+                      type="text"
+                      v-model="form.password"
                       required
-                      v-model="form.food">
-        </b-form-select>
+                      placeholder="Enter password">
+        </b-form-input>
+
       </b-form-group>
-      <b-form-group id="exampleGroup4">
-        <b-form-checkbox-group v-model="form.checked" id="exampleChecks">
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+
+      <div class="text-center">
+        <b-form-group id="exampleGroup4">
+          <b-form-checkbox-group v-model="form.checked" id="exampleChecks">
+            <b-form-checkbox value="forget">Forget me</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button class="btn-success" type="submit" variant="primary">Log in</b-button>
+    </div>
     </b-form>
   </div>
 </template>
@@ -49,8 +44,8 @@ export default {
   data() {
     return {
       form: {
-        email: '',
-        name: '',
+        username: '',
+        password: '',
         food: null,
         checked: [],
       },
@@ -85,5 +80,13 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+  .auth {
+    margin-top: 10%;
+  }
+
+</style>
 
 <!-- b-form-1.vue -->
